da74bd98d1be71f8632a9444b36f11eeca6e217b
Motus971 <56416121+Motus971@users.noreply.github.com>
2021-12-02 10:49:52 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,13 +1,4 @@
-/**
- * Domain classes used to produce .....
- * <p>
- * These classes contain the ......
- * </p>
- *
- * @since 1.0
- * @author somebody
- * @version 1.0
- */
+@DefaultAnnotation(NonNull.class)
 package com.example;
 
 /**
