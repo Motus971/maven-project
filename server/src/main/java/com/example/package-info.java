2e3044a85b84e602947a2b592413e3f18e3d150c
Motus971 <56416121+Motus971@users.noreply.github.com>
2021-12-02 10:55:49 +0100

    Create package-info.java

---
+++ server/src/main/java/com/example/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Domain classes used to produce the JSON and XML output for the RESTful services. 
+ * <p>
+ * Free Gwada
+ *
+ * @since 1.0
+ * @author Lionel
+ * @version 1.1
+ */
+package com.example;
